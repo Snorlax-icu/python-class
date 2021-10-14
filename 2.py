4ec8d51e9431607f93b192b75169719ef2533cf2
lizard <tomprisoner@foxmail.com>
2021-10-14 18:01:37 +0800

    main

---
+++ 2.py
@@ -0,0 +1,2 @@
+step = input("我们走多少步呀？")
+print(f"我们走{step}步")
---
+++ 有趣代码.py
@@ -0,0 +1,13 @@
+# 二进制转十进制
+decimal = int('1010', 2)
+print(decimal)
+
+# 将字符串转换为 小写
+print("hi xiaoming, PLEASE STOP!".lower())
+# 将字符串转换为 大写
+print("hi xiaoming, PLEASE STOP!".upper())
+# 将字符串转换为 字节
+print("hi xiaoming, PLEASE STOP!".encode())
+# 复制文件
+import shutil
+shutil.copyfile('1.py','2.py')
